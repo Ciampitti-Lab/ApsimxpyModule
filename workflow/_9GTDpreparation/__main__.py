a90d98f931a64dc0977ddefcd7ec97b679c08810
JorgeJola <120377392+JorgeJola@users.noreply.github.com>
2026-01-22 15:09:16 -0500

    chore: grouping trials gtd1

--- workflow/_9GTDpreparation/__main__.py
+++ workflow/_9GTDpreparation/__main__.py
@@ -28,7 +28,59 @@ region3_map = {
 ###############################################################################################################################
 #                                                   NRCS N Project Indiana                                                    #
 ###############################################################################################################################
+# Ground truth data (NRCS)
+gtd1=pd.read_csv("/GTD1.csv")
 
+# Filtering dataset by previous crop (Only Soybean)
+gtd1=gtd1[gtd1['PreCrop']=='Soybean']
+
+# Deleting no necessary columns 
+gtd1.drop(['State','FieldName','PlotID','GridID','Trt_n','Grid_org','Transects_n','TransectID','Transect_R',
+           'Trans_A','Trans_B','Strategy','PNM','FP','Damage','Area_ac','RepRefB','Pt_NPass','Sd_NPass','Hybrid',
+           'Tile','PreCrop','Tillage','FNR_lbac','rExN_lbac','rStN_lbac','pPreN_lbac','rPreN_lbac','pSidN_lbac','SidN_lbac',
+           'rSdN_lbac','pTotN_lbac','rTN_lbac','rYl15_buac'],axis=1,inplace=True)
+# Fixing Years
+gtd1['Year']=gtd1['Year']+2000
+
+# Setting nitrogen rates
+gtd1["Nitrogen"] = gtd1["rTotN_kgha"].round(2)
+
+# Fixing counties
+gtd1["county"] = gtd1["County"].replace({
+    "White": "White County",
+    "Randolph": "Randolph County",
+    "Dubois": "Dubois County",
+    "Marshall": "Marshall County",
+    "Clay": "Clay County"
+})
+
+# Grouping trials 
+new_gtd1 = (gtd1.groupby(["county", "Year", "Field", "FieldID", "Treatment", "Ref_block", "Nitrogen"],as_index=False).agg(Yield=("rY15_mtha", "mean")))
+
+# Filtering trials with less than 3 nitrogen rates
+new_gtd1 = new_gtd1[new_gtd1.groupby(["FieldID", "Treatment", "Ref_block"])["Nitrogen"].transform("count") >= 3]
+
+# ID for each trial (Combination of FieldID, Treatment and Ref_block)
+new_gtd1["id_trial"] = (
+    new_gtd1
+    .groupby(["FieldID", "Treatment", "Ref_block"])
+    .ngroup()
+)
+
+# Setting region
+new_gtd1["region"] = new_gtd1["county"].map(region3_map)
+
+# GTD label
+gtd_label=[]
+for idx,row in new_gtd1.iterrows():
+    gtd_label.append('GTD1')
+    
+new_gtd1['dbase']=gtd_label
+
+# Deleting no necessary columns 
+new_gtd1.drop(['Ref_block','Treatment','Field','FieldID'],axis=1,inplace=True)
+
+new_gtd1.to_csv('/new_gtd1.csv',index=False)
 
 # Still working in find the trials.
 
@@ -44,7 +96,7 @@ region3_map = {
 # Ground truth data (Master Database)
 gtd2=pd.read_csv("/GTD2.csv")
 
-# Setting rates of nitrogen (Bushes) (These are the values we will get the yield from the curves)
+# Setting rates of nitrogen (These are the values we will get the yield from the curves)
 nitro = np.random.uniform(0, 267.66, size=100)
 
 # Variables of the new GTD2
@@ -102,11 +154,11 @@ for idx,row in gtd2.iterrows():
 
 # Creating new GTD2
 new_gtd2=pd.DataFrame({
-    'yield':yield_ton,
-    'nitrogen':NKg_Ha,
+    'Yield':yield_ton,
+    'Nitrogen':NKg_Ha,
     'county':county,
     'region':region,
-    'year':year,
+    'Year':year,
     'id_trial':id_trial,
     'dbase':dbase
     })
@@ -120,4 +172,4 @@ new_gtd2["county"] = new_gtd2["county"].replace(county_map)
 
 new_gtd2["region"] = new_gtd2["county"].map(region3_map)
 
-# new_gtd2.to_csv('/new_gtd2.csv',index=False)
+new_gtd2.to_csv('/new_gtd2.csv',index=False)
